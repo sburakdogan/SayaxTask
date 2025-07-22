1af217b58d1753184c3d99e16da5f051d6cea3cb
Burak Dogan <46339962+sburakdogan@users.noreply.github.com>
2025-07-22 20:34:22 +0300

    cors

--- SayaxTask.Api/Program.cs
+++ SayaxTask.Api/Program.cs
@@ -4,6 +4,16 @@ using SayaxTask.Business.Helper;
 using SayaxTask.Business.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 // Add services to the container.
 
@@ -29,7 +39,7 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
+app.UseCors("AllowAll");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
